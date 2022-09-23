576661ff0c341b72c74aa1704f8358ad2b23e8df
SiewertDekens <102385698+SiewertDekens@users.noreply.github.com>
2022-09-23 13:37:13 +0200

    Beer-Tracker map

---
+++ Beer-Tracker/Beer-Tracker/Beer_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Beer_TrackerApp.swift
+//  Beer-Tracker
+//
+//  Created by Siewert Dekens on 23/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Beer_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
